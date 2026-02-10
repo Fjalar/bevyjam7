b06bd409425d93a3c4f42ab85d678eb79a23e5fe
Fjalar <fjalarhognason@gmail.com>
2026-02-10 21:21:10 +0100

    Fix background depth, ZIndex is actually for UI elements (which are always drawn on top anyway

--- src/demo/level.rs
+++ src/demo/level.rs
@@ -35,8 +35,7 @@ impl FromWorld for LevelAssets {
 pub fn background_bundle(level_assets: Res<LevelAssets>) -> impl Bundle {
     (
         Sprite::from_image(level_assets.background.clone()),
-        Transform::from_scale(Vec2::splat(1.0).extend(1.0)),
-        ZIndex(-1),
+        Transform::from_scale(Vec2::splat(1.0).extend(-1.0)),
     )
 }
 
