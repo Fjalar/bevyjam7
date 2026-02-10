98d2256f84eac2108c8eee45a0a2229ae0381c80
Fjalar <fjalarhognason@gmail.com>
2026-02-11 00:29:28 +0100

    Bullets from the gun

--- src/demo/gun.rs
+++ src/demo/gun.rs
@@ -1,5 +1,3 @@
-use std::f32::consts::PI;
-
 use bevy::image::{ImageLoaderSettings, ImageSampler};
 use bevy::prelude::*;
 use bevy::window::PrimaryWindow;
@@ -17,22 +15,32 @@ pub(super) fn plugin(app: &mut App) {
                 .in_set(AppSystems::RecordInput)
                 .in_set(PausableSystems),
         ),
-    );
+    )
+    .add_systems(FixedUpdate, update_bullet);
 }
 
 #[derive(Resource, Asset, Clone, Reflect)]
 #[reflect(Resource)]
 pub struct GunAssets {
     #[dependency]
-    image: Handle<Image>,
+    gun: Handle<Image>,
+    #[dependency]
+    bullet: Handle<Image>,
 }
 
 impl FromWorld for GunAssets {
     fn from_world(world: &mut World) -> Self {
         let assets = world.resource::<AssetServer>();
         Self {
-            image: assets.load_with_settings(
+            gun: assets.load_with_settings(
                 "images/gun.png",
+                |settings: &mut ImageLoaderSettings| {
+                    // Use `nearest` image sampling to preserve pixel art style.
+                    settings.sampler = ImageSampler::nearest();
+                },
+            ),
+            bullet: assets.load_with_settings(
+                "images/bullet.png",
                 |settings: &mut ImageLoaderSettings| {
                     // Use `nearest` image sampling to preserve pixel art style.
                     settings.sampler = ImageSampler::nearest();
@@ -44,7 +52,7 @@ impl FromWorld for GunAssets {
 
 pub fn gun_bundle(gun_assets: &GunAssets) -> impl Bundle {
     (
-        Sprite::from_image(gun_assets.image.clone()),
+        Sprite::from_image(gun_assets.gun.clone()),
         Gun::default(),
         Transform::from_xyz(32.0, 0.0, 0.0),
     )
@@ -61,30 +69,46 @@ fn update_gun(
 ) {
     if let Some(position) = window.cursor_position() {
         let mouse_vector = position - Vec2::new(window.width() / 2.0, window.height() / 2.0);
-        let angle = (-mouse_vector.y / mouse_vector.x).atan();
+        let angle = Vec2::new(mouse_vector.x, -mouse_vector.y).to_angle();
         gun.0.angle = angle;
         gun.1.translation = Vec3::X * 32.0;
         gun.1.rotation = Quat::default();
         gun.1
             .rotate_around(Vec3::ZERO, Quat::from_rotation_z(angle));
-        if mouse_vector.x.is_sign_negative() {
-            gun.1.rotate_around(Vec3::ZERO, Quat::from_rotation_z(PI));
-            gun.2.flip_y = true;
-        } else {
-            gun.2.flip_y = false;
-        }
+        gun.2.flip_y = mouse_vector.x.is_sign_negative();
     }
 }
 
 fn shoot_gun(
     mut commands: Commands,
     mouse: Res<ButtonInput<MouseButton>>,
+    gun_assets: Res<GunAssets>,
     gun: Single<(&GlobalTransform, &Gun)>,
 ) {
     if mouse.just_pressed(MouseButton::Left) {
-        commands.spawn((
-            Sprite::from_color(Color::WHITE, Vec2::splat(32.0)),
+        commands.spawn(bullet_bundle(
+            &gun_assets,
             Transform::from_translation(gun.0.translation()).with_rotation(gun.0.rotation()),
+            Vec2::from_angle(gun.1.angle) * 320.0,
         ));
+    }
+}
+
+#[derive(Component)]
+struct Bullet {
+    velocity: Vec2,
+}
+
+fn bullet_bundle(gun_assets: &GunAssets, transform: Transform, velocity: Vec2) -> impl Bundle {
+    (
+        Sprite::from_image(gun_assets.bullet.clone()),
+        Bullet { velocity },
+        transform,
+    )
+}
+
+fn update_bullet(bullets: Query<(&mut Transform, &Bullet)>, time: Res<Time>) {
+    for (mut transform, bullet) in bullets {
+        transform.translation += bullet.velocity.extend(0.0) * time.delta_secs()
     }
 }
